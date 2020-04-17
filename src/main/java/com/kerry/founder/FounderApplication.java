b220d205f7beadd19dc47cba131163cd938591e4
hekai <traurigekatze@163.com>
2020-04-17 14:15:54 +0800

    first commit

---
+++ src/main/java/com/kerry/founder/FounderApplication.java
@@ -0,0 +1,13 @@
+package com.kerry.founder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FounderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FounderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerry/founder/FounderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerry.founder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FounderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
